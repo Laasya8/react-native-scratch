b95dbf0dd80ec12e042ca7b98d49c1550847327f
Laasya8 <laasya1106@gmail.com>
2026-08-15 15:29:47 +0530

    implemented custom fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,42 +9,40 @@ const SafeAreaView=styled(RNSafeAreaView);
 export default function App() {
     return (
         <SafeAreaView className="flex-1 bg-background p-5">
-            <Text className="text-xl font-bold text-orange-400">
-                Welcome to my first project!!
-            </Text>
+            <Text className="text-5xl font-sans-bold ">Home </Text>
             <Link href="/onboarding" asChild>
-                <Text className="mt-4 rounded bg-[#081126] text-white p-4">
+                <Text className="mt-4 rounded font-sans-bold bg-[#081126] text-white p-4">
                     Go to Onboarding
                 </Text>
             </Link>
             <Link href="/(auth)/sign-in" asChild>
-                <Text className="mt-4 rounded bg-[#081126] text-white p-4">
+                <Text className="mt-4 rounded font-sans-bold bg-[#081126] text-white p-4">
                     Go to Sign In
                 </Text>
             </Link>
             <Link href="/(auth)/sign-up" asChild>
-                <Text className="mt-4 rounded bg-[#081126] text-white p-4">
+                <Text className="mt-4 rounded font-sans-bold bg-[#081126] text-white p-4">
                     Go to Sign Up
                 </Text>
             </Link>
-            <Link
-                href={{
-                    pathname:"/subscriptions/[id]",
-                    params:{id:"spotify"},
-                }}
-                asChild
-            >
-                <Text className="mt-4">Spotify subscription</Text>
-            </Link>
-            <Link
-                href={{
-                    pathname:"/subscriptions/[id]",
-                    params:{id:"claude"},
-                }}
-                asChild
-            >
-                <Text className="mt-4">Claude Subscription</Text>
-            </Link>
+            {/*<Link*/}
+            {/*    href={{*/}
+            {/*        pathname:"/subscriptions/[id]",*/}
+            {/*        params:{id:"spotify"},*/}
+            {/*    }}*/}
+            {/*    asChild*/}
+            {/*>*/}
+            {/*    <Text className="mt-4">Spotify subscription</Text>*/}
+            {/*</Link>*/}
+            {/*<Link*/}
+            {/*    href={{*/}
+            {/*        pathname:"/subscriptions/[id]",*/}
+            {/*        params:{id:"claude"},*/}
+            {/*    }}*/}
+            {/*    asChild*/}
+            {/*>*/}
+            {/*    <Text className="mt-4">Claude Subscription</Text>*/}
+            {/*</Link>*/}
         </SafeAreaView>
     );
 }
